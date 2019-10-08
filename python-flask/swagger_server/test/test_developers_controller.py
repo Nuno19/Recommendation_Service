f4346fcc4086003b5140dbc20e4e47c9cf69fa96
Nuno19 <capela.nuno@ua.pt>
2019-10-08 09:16:36 +0000

    Generated code by SwaggerHub

--- python-flask/swagger_server/test/test_developers_controller.py
+++ python-flask/swagger_server/test/test_developers_controller.py
@@ -12,6 +12,19 @@ from swagger_server.test import BaseTestCase
 class TestDevelopersController(BaseTestCase):
     """DevelopersController integration test stubs"""
 
+    def test_create_user(self):
+        """Test case for create_user
+
+        createUser
+        """
+        query_string = [('extID', 'extID_example')]
+        response = self.client.open(
+            '/Nuno19/Recomendation_Service/1.0.0/createUser',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
     def test_get_recommended(self):
         """Test case for get_recommended
 
