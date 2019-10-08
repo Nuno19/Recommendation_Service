076c720f239eb7ab8fd7fc01ca7eba8e33920a9f
Nuno19 <capela.nuno@ua.pt>
2019-10-08 08:37:31 +0000

    Generated code by SwaggerHub

---
+++ python-flask/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.cinema_item import CinemaItem
+from swagger_server.models.item import Item
---
+++ python-flask/swagger_server/test/test_developers_controller.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.item import Item  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestDevelopersController(BaseTestCase):
+    """DevelopersController integration test stubs"""
+
+    def test_get_recommended(self):
+        """Test case for get_recommended
+
+        searches recommended
+        """
+        query_string = [('maxCount', 56),
+                        ('movieId', 1)]
+        response = self.client.open(
+            '/Nuno19/Recomendation_Service/1.0.0/getRecommended',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
