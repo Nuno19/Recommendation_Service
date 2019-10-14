809436a51dad5daf9c1255d920b6fa22cba23a6c
Nuno19 <capela.nuno@ua.pt>
2019-10-14 16:49:45 +0000

    Generated code by SwaggerHub

--- python-flask/swagger_server/controllers/developers_controller.py
+++ python-flask/swagger_server/controllers/developers_controller.py
@@ -5,19 +5,6 @@ from swagger_server.models.item import Item  # noqa: E501
 from swagger_server import util
 
 
-def create_user(extID):  # noqa: E501
-    """createUser
-
-    By passing in the appropriate options, you can search for available cinemas  # noqa: E501
-
-    :param extID: 
-    :type extID: str
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-
 def get_recommended(maxCount=None, movieId=None):  # noqa: E501
     """searches recommended
 
--- python-flask/swagger_server/models/__init__.py
+++ python-flask/swagger_server/models/__init__.py
@@ -4,4 +4,3 @@
 from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.item import Item
-from swagger_server.models.user_item import UserItem
