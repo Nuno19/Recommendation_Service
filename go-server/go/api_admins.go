2f0db3e5727aed8d878585237d8914da7a2566c6
Nuno19 <capela.nuno@ua.pt>
2019-10-22 09:15:33 +0000

    Generated code by SwaggerHub

--- go-server/go/api_admins.go
+++ go-server/go/api_admins.go
@@ -17,7 +17,6 @@ import (
 func LoadItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-
 }
 
 func LoadItemList(w http.ResponseWriter, r *http.Request) {
